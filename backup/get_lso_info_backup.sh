cadc5259a8c2b64e0d80614aafef9d950d24f1ec
hoffoh <hofillo@hotmail.com>
2025-09-18 11:35:49 +0200

    Optimized lso script

--- backup/get_lso_info_backup.sh
+++ backup/get_lso_info_backup.sh
@@ -10,9 +10,16 @@ ITEMS=(
 "lsblk"
 "ls -lR /dev/disk/by-*"
 "ls -lR /mnt/local-storage/"
-"head --bytes=60 /dev/sd[a-z]"
-"head --bytes=60 /dev/vd[a-z]"
-"head --bytes=60 /dev/nvme[0-9]"
+"head --bytes=60 /dev/sda"
+"head --bytes=60 /dev/sdb"
+"head --bytes=60 /dev/sdc"
+"head --bytes=60 /dev/sdd"
+"head --bytes=60 /dev/sde"
+"head --bytes=60 /dev/sdf"
+"head --bytes=60 /dev/sdg"
+"head --bytes=60 /dev/sdh"
+"head --bytes=60 /dev/sdi"
+"head --bytes=60 /dev/sdj"
 ) 
 
 # Control the field separator for array spaces
@@ -24,7 +31,7 @@ collect_data () {
 for i in ${ITEMS[@]}
 do echo '' >> $FILENAME
         echo "#> $i" >> $FILENAME
-        eval "$i" >> $FILENAME 2> /dev/null
+        eval "$i" >> $FILENAME
 done
 }
  
