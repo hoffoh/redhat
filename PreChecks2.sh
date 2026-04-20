90c4b68d4c19a3d3c2a092b0e560fd7fdafcf6f7
Rafael Rojas <rafrojas@supportshell-1.sush-001.prod.eu-central-1.aws.redhat.com>
2026-04-20 15:15:39 +0000

    Beta version, corrected OSD alignement added IOPS

--- PreChecks2.sh
+++ PreChecks2.sh
@@ -139,14 +139,15 @@ function get_odf (){
   X=17;print_title
   omc get pods -o wide | egrep -v "Running|Completed" >> $FILE
   X=10;print_title
-  echo -e "POD_NAME\t\t\t\t\t\t\t\tPVC\t\t\t\t\t\t\t\t\tPV\t\t\t\t\t\t\t\tNODE" >> $FILE
   omc get pods -l app=rook-ceph-osd -o 'custom-columns=NAME:.metadata.name,PVC:.spec.volumes.*.persistentVolumeClaim.claimName,PV:.spec.volumeName,NODE:.spec.nodeName,STATUS:.status.phase' --no-headers | \
    while read -r pod_name pvc_name node status; do
     if [ -n "$pvc_name" ] && [ "$pvc_name" != "N/A" ]; then
        pv_from_pvc=$(omc get pvc "$pvc_name" -o 'custom-columns=PV:.spec.volumeName' --no-headers 2>/dev/null)
        local_path=$(omc get pv "$pv_from_pvc" -o 'custom-columns=PV:.spec.local.path' --no-headers 2>/dev/null)
       if [ -n "$pv_from_pvc" ]; then
+        echo -e "POD_NAME\t\t\t\t\t\t\t\tPVC\t\t\t\t\t\t\t\t\tPV\t\t\t\t\t\t\t\tNODE" >> $FILE
         echo -e "$pod_name\t$pvc_name\t$pv_from_pvc\t$node" >> $FILE
+        echo -e "\tPATH:$local_path\t\t\tSTATUS:$status" >> $FILE && print_space
       else
         echo "  PV for $pvc_name: Not found"
       fi
@@ -154,21 +155,6 @@ function get_odf (){
       echo "Skipping line (PVC name not found or N/A): $pod_name"
     fi
   done && print_space
-  echo -e "POD_NAME\t\t\t\t\t\t\t\tPATH\t\t\t\t\t\t\t\t\t\t\t\t\t\tSTATUS" >> $FILE
-  omc get pods -l app=rook-ceph-osd -o 'custom-columns=NAME:.metadata.name,PVC:.spec.volumes.*.persistentVolumeClaim.claimName,PV:.spec.volumeName,NODE:.spec.nodeName,STATUS:.status.phase' --no-headers | \
-   while read -r pod_name pvc_name node status; do
-    if [ -n "$pvc_name" ] && [ "$pvc_name" != "N/A" ]; then
-       pv_from_pvc=$(omc get pvc "$pvc_name" -o 'custom-columns=PV:.spec.volumeName' --no-headers 2>/dev/null)
-       local_path=$(omc get pv "$pv_from_pvc" -o 'custom-columns=PV:.spec.local.path' --no-headers 2>/dev/null)
-      if [ -n "$pv_from_pvc" ]; then
-        echo -e "$pod_name\t$local_path\t$status" >> $FILE
-      else
-        echo "  PV for $pvc_name: Not found"
-      fi
-    else
-      echo "Skipping line (PVC name not found or N/A): $pod_name"
-    fi
-  done
   get_noobaa
 }
 
